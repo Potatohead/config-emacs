66c04d70bcca4ea04a571ab6d0a13679c37e35e4
Christopher Mckay <potatohead@potatolan.com>
2012-02-17 10:10:48 +1000

    Changed yes-no to y-n

--- init/global.el
+++ init/global.el
@@ -1,11 +1,19 @@
+;;; Generic emacs settings I cannot live without
+
+;; "y or n" instead of "yes or no"
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Relocate backup files
 (defvar backup-dir (expand-file-name "~/.emacs.d/backup/"))
 (setq backup-directory-alist (list (cons ".*" backup-dir)))
 
+;; Set how many backup files to keep
 (setq delete-old-versions t
   kept-new-versions 6
   kept-old-versions 2
   version-control t)
 
+;; autosave files are off
 (setq auto-save-default nil)
 ;; (defvar autosave-dir (expand-file-name "~/.emacs.d/autosave/"))
 ;; (setq auto-save-list-file-prefix autosave-dir)
